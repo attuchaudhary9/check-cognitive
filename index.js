0249a5cfbf928ac7c6fd0ff73c34952141031141
Aatif Ali <attuchaudhary9@gmail.com>
2021-12-28 18:29:59 +0530

    modem service helper 01

--- index.js
+++ index.js
@@ -1,202 +1,69 @@
-const sanitizeGeneralSettingConfigs = (params, siteConstantsConfig) => {
-  const previousFields = [];
-  const changedFields = [];
-  const generalSettingConfigs = [];
 
-  const offlineModemDelCycleParamsCheck = (paramsSetting, settingName) => {
-    const offLineModemCycleDays = [3, 7, 10, 14];
-    paramsSetting = Number(paramsSetting);
-    if (!offLineModemCycleDays.includes(paramsSetting)) {
-      throw new Error(`ERR_INVALID_${settingName}`);
-    }
-  };
+const extractStrategicData = async (params) => {
+  let isAnyFileSaved = false;
 
-  const historyDeleteCycleParamsCheck = (paramsSetting, settingName) => {
-    const historicalDeleteCycleDays = [
-      7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105,
-    ];
-    paramsSetting = Number(paramsSetting);
-    if (!historicalDeleteCycleDays.includes(paramsSetting)) {
-      throw new Error(`ERR_INVALID_${settingName}`);
-    }
-  };
+  try {
+    const dataExtractionTables = await Models.rdbms.DataExtraction.getDataExtractionTables({ is_active: true });
+    const timezone = await Helpers.commonHelper.getSiteConstant('SYSTEM_TIMEZONE');
+    const date = Util.dateTimeUtils.getCurrentTimeBasedOn(timezone);
+    const currentDay = date.getDay();
+    const currentHour = Util.dateTimeUtils.getTimeInHours(timezone);
+    const timestamp = date.getTime();
 
-  const loginAttamptParamsCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting)) throw new Error(`ERR_INVALID_${settingName}`);
-  };
+    const externalRepositories = await Models.rdbms.ExternalRepository.getExternalRepositories({ is_active: true });
+    const isAnyValidBucket = await ModemHelper.isAnyValidBucketExist(externalRepositories);
 
-  const disableNotificationValParamsCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !(paramsSetting === 1 || paramsSetting === 0))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
+    if(!isAnyValidBucket) {
+      Logger.info('The External Repositories provided does not have valid credentials');
+    } else {
+      const result = await ModemHelper.generateStrategicDataTablesCsv({
+        dataExtractionTables,  currentHour, currentDay, timestamp, cronName: params.cron_name
+      });
+      const indexOfModemByCmtsTable = result.indexOf(`modem_by_cmts_${timestamp}.csv`);
 
-  const maxBirthCertificateParamsCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !(paramsSetting >= 1 && paramsSetting <= 50))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
+      /* modem_by_cmts entry swipped with last entry of result array to unblock other CSVs to upload first
+      because csv generated for this table is very large in size
+      and takes lot of time to upload CSV on external repository */
+      if (result && indexOfModemByCmtsTable != -1 && indexOfModemByCmtsTable != result.length - 1) {
+        [result[indexOfModemByCmtsTable], result[result.length - 1]] =
+        [result[result.length - 1], result[indexOfModemByCmtsTable]];
+      }
 
-  const vTDRSensitivityParamsCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !(paramsSetting >= -40 && paramsSetting <= -15))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
+      if (result.length) {
+        for (const repository of externalRepositories) {
+          if (repository.source_name === 'S3') {
+            const { bucketName, decryptedAccessKeyId, decryptedSecretKey } =
+            ModemHelper.getDecryptedBucketCredential(repository);
 
-  const maxUsrxpwrParamsCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (
-      isNaN(paramsSetting) ||
-      !(paramsSetting >= -1000 && paramsSetting <= 1000)
-    )
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
+            const S3 = new AWS.S3({
+              accessKeyId: decryptedAccessKeyId,
+              secretAccessKey: decryptedSecretKey
+            });
 
-  const lengthUnitParamsCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-
-    if (
-      isNaN(paramsSetting) ||
-      !(paramsSetting === 1 || paramsSetting === 3.28)
-    ) {
-      throw new Error(`ERR_INVALID_${settingName}`);
-    }
-  };
-
-  const modemFixesRadiusCheck = (paramsSetting, settingName) => {
-    const validModemFixRadius = [10, 50, 100, 500, 1000];
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !validModemFixRadius.includes(paramsSetting))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
-
-  const correlationFixesRadiusCheck = (paramsSetting, settingName) => {
-    const validCorrFixRadius = [50, 100, 500, 1000, 2000, 5000];
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !validCorrFixRadius.includes(paramsSetting))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
-
-  const pnmMaxCmtsCpuUtilizationCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !(paramsSetting < 100 && paramsSetting > 1))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
-
-  const pnmRepollWaitTimeCheck = (paramsSetting, settingName) => {
-    paramsSetting = Number(paramsSetting);
-    if (isNaN(paramsSetting) || !(paramsSetting < 1320 && paramsSetting > 1))
-      throw new Error(`ERR_INVALID_${settingName}`);
-  };
-
-  [
-    "OFFLINE_MODEM_DELETE_CYCLE",
-    "HISTORY_DELETE_CYCLE",
-    "MAX_THRESHOLD",
-    "MIN_THRESHOLD",
-    "LOGIN_ATTEMPT",
-    "GOOD_PORT_HEALTH_SCORE",
-    "DISABLE_NOTIFICATION_VAL",
-    "SHOW_HIDE_REGISTER_VAL",
-    "LENGTH_UNIT",
-    "CORR_RADIUS",
-    "TOLLERENCE",
-    "MAX_BIRTH_CERTIFICATE",
-    "MAX_usrxpwr",
-    "MIN_usrxpwr",
-    "vTDR_SENSITIVITY",
-    "MODEM_FIXES_RADIUS",
-    "CORRELATION_FIXES_RADIUS",
-    "CMTR_CORR",
-    "NMTR_CORR",
-    "ENABLE_AUTO_GEOCODE",
-    "PNM_MAX_CMTS_CPU_UTILIZATION",
-    "PNM_REPOLL_WAIT_TIME",
-  ].forEach((settingName) => {
-    if (
-      params[settingName] !== null &&
-      typeof params[settingName] !== "undefined"
-    ) {
-      switch (settingName) {
-        case "OFFLINE_MODEM_DELETE_CYCLE": {
-          offlineModemDelCycleParamsCheck(params[settingName], settingName);
-          break;
-        }
-
-        case "HISTORY_DELETE_CYCLE": {
-          historyDeleteCycleParamsCheck(params[settingName], settingName);
-          break;
-        }
-
-        case "MAX_THRESHOLD":
-        case "MIN_THRESHOLD":
-        case "LOGIN_ATTEMPT":
-        case "GOOD_PORT_HEALTH_SCORE":
-        case "CORR_RADIUS":
-        case "TOLLERENCE":
-        case "CMTR_CORR":
-        case "NMTR_CORR": {
-          loginAttamptParamsCheck(params[settingName], settingName);
-          break;
-        }
-        case "DISABLE_NOTIFICATION_VAL":
-        case "SHOW_HIDE_REGISTER_VAL":
-        case "ENABLE_AUTO_GEOCODE": {
-          disableNotificationValParamsCheck(params[settingName], settingName);
-          break;
-        }
-
-        case "MAX_BIRTH_CERTIFICATE": {
-          maxBirthCertificateParamsCheck(params[settingName], settingName);
-          break;
-        }
-
-        case "vTDR_SENSITIVITY": {
-          vTDRSensitivityParamsCheck(params[settingName], settingName);
-
-          break;
-        }
-        case "MAX_usrxpwr":
-        case "MIN_usrxpwr": {
-          maxUsrxpwrParamsCheck(params[settingName], settingName);
-          break;
-        }
-        case "LENGTH_UNIT": {
-          lengthUnitParamsCheck(params[settingName], settingName);
-          break;
-        }
-        case "MODEM_FIXES_RADIUS": {
-          modemFixesRadiusCheck(params[settingName], settingName);
-          break;
-        }
-        case "CORRELATION_FIXES_RADIUS": {
-          correlationFixesRadiusCheck(params[settingName], settingName);
-          break;
-        }
-        case "PNM_MAX_CMTS_CPU_UTILIZATION": {
-          pnmMaxCmtsCpuUtilizationCheck(params[settingName], settingName);
-          break;
-        }
-        case "PNM_REPOLL_WAIT_TIME": {
-          pnmRepollWaitTimeCheck(params[settingName], settingName);
-          break;
+            for (const csvFileName of result) {
+              try {
+                const filePath = `${process.cwd()}/data_extraction/${csvFileName}`;
+                if (Util.commonUtils.doesFileExists(filePath)) {
+                  const key = `data_extraction/${csvFileName}`;
+                  // eslint-disable-next-line no-await-in-loop
+                  await Util.commonUtils.uploadFileToS3(S3, key, bucketName, filePath, csvFileName);
+                  isAnyFileSaved = true;
+                }
+              } catch (error) {
+                Logger.error(`Error while uploading csv for table ${csvFileName} on bucket ${bucketName}`);
+                Logger.error(error);
+              }
+            }
+          }
         }
       }
-      if (Number(siteConstantsConfig[settingName]) !== params[settingName]) {
-        changedFields.push({ [settingName]: params[settingName] });
-        previousFields.push({
-          [settingName]: siteConstantsConfig[settingName],
-        });
-
-        generalSettingConfigs.push({
-          constant_name: settingName,
-          constant_value: params[settingName],
-          status: true,
-        });
-      }
     }
-  });
+  } catch (error) {
+    return { 'error': error };
+  }
 
-  return [generalSettingConfigs, previousFields, changedFields];
+  const message = isAnyFileSaved ? 'Data extraction reports saved successfully!!!' :
+  'Data extraction CRON execution completed without generating any CSV!!!';
+
+  return { 'success': message };
 };
