f38f188dccda03bddc0354d3abda041d098912ae
Aatif Ali <attuchaudhary9@gmail.com>
2021-12-29 17:12:12 +0530

    foreach method apply

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
 const extractStrategicData = async (params) => {
+  // console.log('done');
   let isAnyFileSaved = false;
 
   try {
@@ -12,41 +12,15 @@ const extractStrategicData = async (params) => {
 
     const externalRepositories = await Models.rdbms.ExternalRepository.getExternalRepositories({ is_active: true });
     const isAnyValidBucket = await ModemHelper.isAnyValidBucketExist(externalRepositories);
+/** For of Loop */
 
-    const s3BucketResult = (result) => {
-      for (const repository of externalRepositories) {
-        if (repository.source_name === 'S3') {
-          const { bucketName, decryptedAccessKeyId, decryptedSecretKey } =
-          ModemHelper.getDecryptedBucketCredential(repository);
-
-          const S3 = new AWS.S3({
-            accessKeyId: decryptedAccessKeyId,
-            secretAccessKey: decryptedSecretKey
-          });
-
-          for (const csvFileName of result) {
-            try {
-              const filePath = `${process.cwd()}/data_extraction/${csvFileName}`;
-              if (Util.commonUtils.doesFileExists(filePath)) {
-                const key = `data_extraction/${csvFileName}`;
-                // eslint-disable-next-line no-await-in-loop
-                await Util.commonUtils.uploadFileToS3(S3, key, bucketName, filePath, csvFileName);
-                isAnyFileSaved = true;
-              }
-            } catch (error) {
-              Logger.error(`Error while uploading csv for table ${csvFileName} on bucket ${bucketName}`);
-              Logger.error(error);
-            }
-          }
-        }
-      }
-    }
     if(!isAnyValidBucket) {
       Logger.info('The External Repositories provided does not have valid credentials');
     } else {
       const result = await ModemHelper.generateStrategicDataTablesCsv({
         dataExtractionTables,  currentHour, currentDay, timestamp, cronName: params.cron_name
       });
+
       const indexOfModemByCmtsTable = result.indexOf(`modem_by_cmts_${timestamp}.csv`);
 
       /* modem_by_cmts entry swipped with last entry of result array to unblock other CSVs to upload first
@@ -58,7 +32,71 @@ const extractStrategicData = async (params) => {
       }
 
       if (result.length) {
-       s3BucketResult(result);
+        externalRepositories.forEach(async repository => {
+          if (repository.source_name === 'S3') {
+            const { bucketName, decryptedAccessKeyId, decryptedSecretKey } =
+            ModemHelper.getDecryptedBucketCredential(repository);
+
+            const S3 = new AWS.S3({
+              accessKeyId: decryptedAccessKeyId,
+              secretAccessKey: decryptedSecretKey
+            });
+result.forEach(async csvFileName => {
+  try {
+    const filePath = `${process.cwd()}/data_extraction/${csvFileName}`;
+    if (Util.commonUtils.doesFileExists(filePath)) {
+      const key = `data_extraction/${csvFileName}`;
+      // eslint-disable-next-line no-await-in-loop
+      await Util.commonUtils.uploadFileToS3(S3, key, bucketName, filePath, csvFileName);
+      isAnyFileSaved = true;
+    }
+  } catch (error) {
+    Logger.error(`Error while uploading csv for table ${csvFileName} on bucket ${bucketName}`);
+    Logger.error(error);
+  }
+});
+            // for (const csvFileName of result) {
+            //   try {
+            //     const filePath = `${process.cwd()}/data_extraction/${csvFileName}`;
+            //     if (Util.commonUtils.doesFileExists(filePath)) {
+            //       const key = `data_extraction/${csvFileName}`;
+            //       // eslint-disable-next-line no-await-in-loop
+            //       await Util.commonUtils.uploadFileToS3(S3, key, bucketName, filePath, csvFileName);
+            //       isAnyFileSaved = true;
+            //     }
+            //   } catch (error) {
+            //     Logger.error(`Error while uploading csv for table ${csvFileName} on bucket ${bucketName}`);
+            //     Logger.error(error);
+            //   }
+            // }
+          }
+        });
+        // for (const repository of externalRepositories) {
+        //   if (repository.source_name === 'S3') {
+        //     const { bucketName, decryptedAccessKeyId, decryptedSecretKey } =
+        //     ModemHelper.getDecryptedBucketCredential(repository);
+
+        //     const S3 = new AWS.S3({
+        //       accessKeyId: decryptedAccessKeyId,
+        //       secretAccessKey: decryptedSecretKey
+        //     });
+
+        //     for (const csvFileName of result) {
+        //       try {
+        //         const filePath = `${process.cwd()}/data_extraction/${csvFileName}`;
+        //         if (Util.commonUtils.doesFileExists(filePath)) {
+        //           const key = `data_extraction/${csvFileName}`;
+        //           // eslint-disable-next-line no-await-in-loop
+        //           await Util.commonUtils.uploadFileToS3(S3, key, bucketName, filePath, csvFileName);
+        //           isAnyFileSaved = true;
+        //         }
+        //       } catch (error) {
+        //         Logger.error(`Error while uploading csv for table ${csvFileName} on bucket ${bucketName}`);
+        //         Logger.error(error);
+        //       }
+        //     }
+        //   }
+        // }
       }
     }
   } catch (error) {
