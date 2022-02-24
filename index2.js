e3074fc92e014fd82afb8c66fdc57cd4383e2edf
Aatif Ali <attuchaudhary9@gmail.com>
2022-02-24 18:17:08 +0530

    add hhh

--- index2.js
+++ index2.js
@@ -1,136 +1,25 @@
-
-UsersHelper.createUserWidgetSettingResponse = async (userData) => {
-  let resultData = {};
-  let modules = await Helpers.cacheHelper.get('MODULES');
-  if (Util.commonUtils.isEmpty(modules)) {
-    await Models.rdbms.Module.loadModules();
-    modules = await Helpers.cacheHelper.get('MODULES');
-  }
-  modules = JSON.parse(modules);
-
-  resultData.widget_sequence = userData.UserSetting.widget_sequence;
-  if (Util.commonUtils.isEmpty(resultData.widget_sequence)) {
-    resultData.widget_sequence = await Models.rdbms.UserSetting.getDefaultWidgetSequenceSetting();
-  }
-
-  resultData.user_widget_preference = userData.UserSetting.user_widget_preference;
-  if (Util.commonUtils.isEmpty(resultData.user_widget_preference)) {
-    resultData.user_widget_preference = await Models.rdbms.UserSetting.getDefaultUserWidgetPreferenceSetting();
-  }
-  const widgets = await UsersRepository.getWidgets();
-  const [leftPanelActive, leftPanelInactive, rightPanelActive, rightPanelInactive ] =  UsersHelper.isPanelActiveOrNot(widgets);
-
-  //Removing all the inactive right Panel widget data from final response
-  resultData.widget_sequence.right_panel = resultData.widget_sequence.right_panel.split(',');
-
-  const rPanelResult =  UsersHelper.removeRPanelInactiveW(rightPanelInactive, resultData);
-  resultData = rPanelResult;
-
-  resultData.widget_sequence.right_panel = resultData.widget_sequence.right_panel.toString();
-
-  //Removing all the inactive left Panel widget data from final response
-  resultData.widget_sequence.left_panel = resultData.widget_sequence.left_panel.split(',');
-
-  const lPanelResult = UsersHelper.removeLPanelInActiceW(leftPanelInactive, resultData);
-  resultData = lPanelResult;
-
-  resultData.widget_sequence.left_panel =  resultData.widget_sequence.left_panel.toString();
-
-  const resultRightActive = UsersHelper.rightPanelActiveResult(rightPanelActive, resultData);
-  resultData = resultRightActive;
-
-  leftPanelActive.forEach(widgetName => {
-    if (!(resultData.widget_sequence.left_panel.includes(widgetName))) {
-      resultData.widget_sequence.left_panel = resultData.widget_sequence.left_panel + ',' + widgetName;
-    }
-  });
-  resultData.widget_sequence.right_panel = resultData.widget_sequence.right_panel.split(',');
-  const mapModuleWid = UsersHelper.mapModuleWidget();
-  for (const widgetName in resultData.user_widget_preference) {
-    if (Object.hasOwnProperty.call(resultData.user_widget_preference, widgetName)) {
-      if (!mapModuleWid.filter(widgetNameFilter => widgetNameFilter.widget === widgetName).map(hasWidgetNameModule => hasWidgetNameModule.module).every(widgetNameMatch => modules.find(mod => mod.name === widgetNameMatch))) {
-        delete resultData.user_widget_preference[widgetName];
-        const indexOf = resultData.widget_sequence.right_panel.indexOf(widgetName);
-        resultData.widget_sequence.right_panel.splice(indexOf, 1);
-      }
-    }
-  }
-  resultData.widget_sequence.right_panel = resultData.widget_sequence.right_panel.toString();
-  return resultData;
-};
-
-UsersHelper.isPanelActiveOrNot = (widgets) => {
-  const leftPanelActive = [];
-  const leftPanelInactive = [];
-  const rightPanelActive = [];
-  const rightPanelInactive = [];
-  widgets.forEach(widget => {
-    if(widget.is_active) {
-      if (widget.is_card) {
-        leftPanelActive.push(widget.name);
-      } else {
-        rightPanelActive.push(widget.name);
-      }
+const val = async (jobRoles, params) => {
+  const id = params.id;
+  params.features.forEach(jobRole => {
+    const feature_id = jobRole.feature_id;
+    if(jobRole.sub_features.length) {
+      jobRole.sub_features.forEach(subFeature => {
+        const sub_feature_id = subFeature.sub_feature_id;
+        const jrfi = jobRoles.find(j => {
+          if(j.job_role_id === id && j.feature_id === feature_id && j.sub_feature_id === sub_feature_id) {
+            return j.id;
+          }
+        });
+        subFeature.job_role_feature_id = jrfi.id;
+      });
     } else {
-      if (widget.is_card) {
-        leftPanelInactive.push(widget.name);
-      } else {
-        rightPanelInactive.push(widget.name);
-      }
-    }
-  });
-return [leftPanelActive, leftPanelInactive, rightPanelActive, rightPanelInactive];
-};
-UsersHelper.removeRPanelInactiveW = (rightPanelInactive, resultData) => {
-  let resultDataP = resultData;
-  rightPanelInactive.forEach(widgetName => {
-    delete resultDataP.user_widget_preference[widgetName];
-    const indexOf = resultDataP.widget_sequence.right_panel.indexOf(widgetName);
-    if (indexOf >= 0) {
-      resultDataP.widget_sequence.right_panel.splice(indexOf, 1);
-    }
-  });
-  return resultDataP;
-};
-UsersHelper.removeLPanelInActiceW = (leftPanelInactive, resultData) => {
-  let resultDataP = resultData;
-  leftPanelInactive.forEach(widgetName => {
-    const indexOf = resultDataP.widget_sequence.left_panel.indexOf(widgetName);
-    if (indexOf >= 0) {
-      resultDataP.widget_sequence.left_panel.splice(indexOf, 1);
-    }
-  });
-  return resultDataP;
-};
-UsersHelper.rightPanelActiveResult = (rightPanelActive, resultData) => {
-  let resultDataP = resultData;
-  rightPanelActive.forEach(widgetName => {
-    /* If any widget is newly added widget, by default show that
-    widget to every user until they update the settings for that widget */
-    if (!(widgetName in resultDataP.user_widget_preference)) {
-      /* For every new widget added in future, will need to add setting here
-      for existing users */
-
-      if(['left_panel', 'search', 'fbc_correlation_list', 'rxmer_correlation_list'].includes(widgetName)){
-        resultDataP.user_widget_preference[widgetName] = { is_visible: true };
-        if (['fbc_correlation_list', 'rxmer_correlation_list'].includes(widgetName)) {
-          resultDataP.widget_sequence.right_panel = resultDataP.widget_sequence.right_panel + ',' + widgetName;
+      const jrfi = jobRoles.find(j => {
+        if(j.job_role_id === id && j.feature_id === feature_id) {
+          return j.id;
         }
-      } else {
-        resultDataP.user_widget_preference[widgetName] = { is_visible: false };
-      }
-    } else {
-      if(
-        resultDataP.user_widget_preference[widgetName].is_visible
-        &&
-        !(resultDataP.widget_sequence.right_panel.includes(widgetName))
-      ) {
-        if(!['left_panel', 'search'].includes(widgetName)){
-          resultDataP.user_widget_preference[widgetName].is_visible = false;
-        }
-      }
+      });
+      jobRole.job_role_feature_id = jrfi.id;
     }
-
   });
-  return resultDataP;
+  return params;
 };
--- test.js
+++ test.js
@@ -1,32 +1,171 @@
-UsersHelper.rightPanelActiveResult = (rightPanelActive, resultData) => {
-  let resultDataP = resultData;
-  
-  rightPanelActive.forEach(widgetName => {
-    /* If any widget is newly added widget, by default show that
-    widget to every user until they update the settings for that widget */
-    if (!(widgetName in resultDataP.user_widget_preference)) {
-      /* For every new widget added in future, will need to add setting here
-      for existing users */
+const val = {
+  "data": {
+      "widget_sequence": {
+          "left_panel": "red_cm_count,red_cm_daily,modem_count,cmts_count,health,red_cm_weekly,alarm,prediction",
+          "right_panel": "modem_action_list,modems_weekly_variation,correlation,spectra_impaired_modems,cmts_cm_status_variation,modem_current_count,birth_certificate_list,work_order_list,top_technician_login,modem_watch_list,fbc_correlation_list,rxmer_correlation_list"
+      },
+      "user_widget_preference": {
+          "node_action_list": {
+              "is_visible": true,
+              "num_records": 3
+          },
+          "modem_action_list": {
+              "is_visible": true,
+              "num_records": 3,
+              "sort_col": "MTC"
+          },
+          "modems_weekly_variation": {
+              "is_visible": true,
+              "num_days": 7
+          },
+          "top_technician_login": {
+              "is_visible": true,
+              "technician_count": 3,
+              "past_days": 7
+          },
+          "work_order_list": {
+              "is_visible": true,
+              "open": true,
+              "in_progress": true,
+              "fixed": true,
+              "closed": true,
+              "assigned": true,
+              "queued": true,
+              "number_of_records": 100
+          },
+          "modems_status_variation": {
+              "is_visible": true,
+              "num_days": 7
+          },
+          "birth_certificate_list": {
+              "is_visible": true,
+              "pass": true,
+              "fail": true,
+              "startDate": "",
+              "endDate": ""
+          },
+          "spectra_impaired_modems": {
+              "is_visible": true
+          },
+          "cmts_cm_status_variation": {
+              "is_visible": true,
+              "num_days": 7
+          },
+          "modem_watch_list": {
+              "is_visible": true
+          },
+          "correlation": {
+              "is_visible": true,
+              "impacted_customer_on_account": true
+          },
+          "modem_current_count": {
+              "is_visible": true
+          },
+          "search": {
+              "is_visible": true
+          },
+          "left_panel": {
+              "is_visible": false
+          },
+          "fbc_correlation_list": {
+              "is_visible": true
+          },
+          "rxmer_correlation_list": {
+              "is_visible": true
+          }
+      }
+  }
+}
 
-      if(['left_panel', 'search', 'fbc_correlation_list', 'rxmer_correlation_list'].includes(widgetName)){
-        resultDataP.user_widget_preference[widgetName] = { is_visible: true };
-        if (['fbc_correlation_list', 'rxmer_correlation_list'].includes(widgetName)) {
-          resultDataP.widget_sequence.right_panel = resultDataP.widget_sequence.right_panel + ',' + widgetName;
-        }
-      } else {
-        resultDataP.user_widget_preference[widgetName] = { is_visible: false };
+const val1 = {
+  "data": {
+      "widget_sequence": {
+          "left_panel": "red_cm_count,red_cm_daily,modem_count,cmts_count,health,red_cm_weekly,alarm,prediction",
+          "right_panel": "modem_action_list,modems_weekly_variation,correlation,spectra_impaired_modems,cmts_cm_status_variation,modem_current_count,birth_certificate_list,work_order_list,top_technician_login,modem_watch_list,fbc_correlation_list,rxmer_correlation_list"
+      },
+      "user_widget_preference": {
+          "node_action_list": {
+              "is_visible": true,
+              "num_records": 3
+          },
+          "modem_action_list": {
+              "is_visible": true,
+              "num_records": 3,
+              "sort_col": "MTC"
+          },
+          "modems_weekly_variation": {
+              "is_visible": true,
+              "num_days": 7
+          },
+          "top_technician_login": {
+              "is_visible": true,
+              "technician_count": 3,
+              "past_days": 7
+          },
+          "work_order_list": {
+              "is_visible": true,
+              "open": true,
+              "in_progress": true,
+              "fixed": true,
+              "closed": true,
+              "assigned": true,
+              "queued": true,
+              "number_of_records": 100
+          },
+          "modems_status_variation": {
+              "is_visible": true,
+              "num_days": 7
+          },
+          "birth_certificate_list": {
+              "is_visible": true,
+              "pass": true,
+              "fail": true,
+              "startDate": "",
+              "endDate": ""
+          },
+          "spectra_impaired_modems": {
+              "is_visible": true
+          },
+          "cmts_cm_status_variation": {
+              "is_visible": true,
+              "num_days": 7
+          },
+          "modem_watch_list": {
+              "is_visible": true
+          },
+          "correlation": {
+              "is_visible": true,
+              "impacted_customer_on_account": true
+          },
+          "modem_current_count": {
+              "is_visible": true
+          },
+          "search": {
+              "is_visible": true
+          },
+          "left_panel": {
+              "is_visible": false
+          },
+          "fbc_correlation_list": {
+              "is_visible": true
+          },
+          "rxmer_correlation_list": {
+              "is_visible": true
+          }
       }
-    } else {
-      if(
-        resultDataP.user_widget_preference[widgetName].is_visible
-        &&
-        !(resultDataP.widget_sequence.right_panel.includes(widgetName))
-      ) {
-        if(!['left_panel', 'search'].includes(widgetName)){
-          resultDataP.user_widget_preference[widgetName].is_visible = false;
-        }
-      }
+  }
+}
+function shallowEqual(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
     }
-
-  });
-};
+  }
+  return true;
+}
+console.log(shallowEqual(val, val1))
